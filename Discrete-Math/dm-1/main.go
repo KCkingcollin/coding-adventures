2a09ca97ee4913733e20f73b5159f92e7702a303
KCkingcollin <kckingcollin99@gmail.com>
2025-01-26 17:52:08 -0500

    Update

--- Discrete-Math/dm-1/main.go
+++ Discrete-Math/dm-1/main.go
@@ -81,13 +81,11 @@ func main() {
     fmt.Println()
 	p = true
     q = true
-    fmt.Println("Truth table for p->q (if p {q} el {true})")
+    fmt.Println("Truth table for p->q (!p || q)")
     fmt.Println("p    |  q  |")
 	for i, j := 1, 1; i <= int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)))); i, j = i+1, j+1 {
         fmt.Println("-------------------------")
-        var a bool = true
-        if p {a = q}
-        fmt.Printf("%t | %t | %t\n", p, q, a)
+        fmt.Printf("%t | %t | %t\n", p, q, !p || q)
         q = !q
         if j == int(math.Pow(2, float64(unsafe.Sizeof(p)))) {
             p = !p
@@ -98,13 +96,11 @@ func main() {
     fmt.Println()
 	p = true
     q = true
-    fmt.Println("Truth table for q->p (if q {p} el {true})")
+    fmt.Println("Truth table for q->p (!q || p)")
     fmt.Println("q    |  p  |")
 	for i, j := 1, 1; i <= int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)))); i, j = i+1, j+1 {
         fmt.Println("-------------------------")
-        var a bool = true
-        if q {a = p}
-        fmt.Printf("%t | %t | %t\n", q, p, a)
+        fmt.Printf("%t | %t | %t\n", q, p, !q || p)
         p = !p
         if j == int(math.Pow(2, float64(unsafe.Sizeof(q)))) {
             q = !q
@@ -115,13 +111,26 @@ func main() {
     fmt.Println()
 	p = true
     q = true
-    fmt.Println("Truth table for ~p->~q (if !p {!q} el {true})")
+    fmt.Println("Truth table for ~p->~q (p || !q)")
     fmt.Println("p    |  q  |")
 	for i, j := 1, 1; i <= int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)))); i, j = i+1, j+1 {
         fmt.Println("-------------------------")
-        var a bool = true
-        if !p {a = !q}
-        fmt.Printf("%t | %t | %t\n", p, q, a)
+        fmt.Printf("%t | %t | %t\n", p, q, p || !q)
+        q = !q
+        if j == int(math.Pow(2, float64(unsafe.Sizeof(p)))) {
+            p = !p
+            j = 0
+        }
+    }
+
+    fmt.Println()
+	p = true
+    q = true
+    fmt.Println("Truth table for p<->q (p == q)")
+    fmt.Println("p    |  q  |")
+	for i, j := 1, 1; i <= int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)))); i, j = i+1, j+1 {
+        fmt.Println("-------------------------")
+        fmt.Printf("%t | %t | %t\n", p, q, p == q)
         q = !q
         if j == int(math.Pow(2, float64(unsafe.Sizeof(p)))) {
             p = !p
