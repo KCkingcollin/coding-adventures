d57c3b0ea0c74dce4ddccc311af0837d8803dd3d
KCkingcollin <kckingcollin99@gmail.com>
2025-01-26 19:03:47 -0500

    Update

--- Discrete-Math/dm-1/main.go
+++ Discrete-Math/dm-1/main.go
@@ -143,17 +143,17 @@ func main() {
     q = true
     var r bool = true
     fmt.Println("Truth table for pvq->~r (!(p || q) || !r)")
-    fmt.Println("q    |  p    |    r   |    pvq   |   ~r   |    pvq->~r")
+    fmt.Println("  p  |  q  |  r  |  pvq  |  ~r  |  pvq->~r")
 	for i, j, l := 1, 1, 1; i <= int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)) + float64(unsafe.Sizeof(r)))); i, j, l = i+1, j+1, l+1 {
         fmt.Println("--------------------------------------------------------")
         fmt.Printf("%t | %t | %t | %t | %t | %t\n", q, p, r, p || q, !r, !(p || q) || !r)
-        p = !p
-        if j == int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)))) {
-            q = !q
+        r = !r
+        if j == int(math.Pow(2, float64(unsafe.Sizeof(p)))) {
+            p = !p
             j = 0
         }
-        if l == int(math.Pow(2, float64(unsafe.Sizeof(q)))) {
-            r = !r
+        if l == int(math.Pow(2, float64(unsafe.Sizeof(q)) + float64(unsafe.Sizeof(p)))) {
+            q = !q
             l = 0
         }
     }
